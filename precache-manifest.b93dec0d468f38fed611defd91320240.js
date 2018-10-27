c96f185f501af4a7470e10c37f31270302262431
Zuhair Taha <admin@tahasoft.com>
2018-10-27 21:08:00 +0200

    Updates

---
+++ precache-manifest.b93dec0d468f38fed611defd91320240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25ec9d39a6d4e622556a",
+    "url": "/react-hw1-todo-app/client/static/js/runtime~main.25ec9d39.js"
+  },
+  {
+    "revision": "f271fb744ab5337cd26c",
+    "url": "/react-hw1-todo-app/client/static/js/main.f271fb74.chunk.js"
+  },
+  {
+    "revision": "531f94847c04bbc4cfe7",
+    "url": "/react-hw1-todo-app/client/static/js/1.531f9484.chunk.js"
+  },
+  {
+    "revision": "f271fb744ab5337cd26c",
+    "url": "/react-hw1-todo-app/client/static/css/main.3732618f.chunk.css"
+  },
+  {
+    "revision": "8f8d536bd4c21c944ca1cf04c7259370",
+    "url": "/react-hw1-todo-app/client/index.html"
+  }
+];
